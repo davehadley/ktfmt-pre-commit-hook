f0f5e1e345f5f2e69d378f8c8cdc2bc0af1399bc
David Hadley <davehadley@users.noreply.github.com>
2021-09-29 13:29:05 +0100

    First working version of ktfmtprecommithook.

---
+++ src/ktfmtprecommithook/__main__.py
@@ -0,0 +1,3 @@
+import ktfmtprecommithook
+
+ktfmtprecommithook._main()
---
+++ src/ktfmtprecommithook/ktfmtprecommithook.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+import urllib.request
+from pathlib import Path
+from subprocess import run
+from typing import Iterable, Optional
+
+DEFAULT_KTFMT_VERSION = "0.28"
+
+
+def ktfmtprecommithook(
+    input_files: Iterable[Path],
+    version: str = DEFAULT_KTFMT_VERSION,
+    extra_args: Optional[Iterable[str]] = None,
+) -> None:
+    if extra_args is None:
+        extra_args = []
+    ktfmt = _get_ktfmt(version=version)
+    cmd = (
+        ["java", "-jar", str(ktfmt)] + list(extra_args) + [str(f) for f in input_files]
+    )
+    run(cmd, check=True)
+    return
+
+
+def _get_ktfmt(version: str) -> Path:
+    try:
+        dst = _cache_directory() / f"ktfmt-{version}-jar-with-dependencies.jar"
+        if not dst.exists():
+            url = f"https://search.maven.org/remotecontent?filepath=com/facebook/ktfmt/{version}/ktfmt-{version}-jar-with-dependencies.jar"
+            tempfilename, _ = urllib.request.urlretrieve(url=url)
+            shutil.move(tempfilename, dst)
+        assert dst.exists()
+    except Exception as ex:
+        raise Exception(f"failed to find ktfmt version {version}") from ex
+    return dst
+
+
+def _cache_directory() -> Path:
+    # try the same places that pre-commit tries https://github.com/pre-commit/pre-commit/blob/master/pre_commit/store.py
+    try:
+        precommitcachedirectory = Path(os.environ.get("PRE_COMMIT_HOME"))  # type: ignore
+    except TypeError:
+        precommitcachedirectory = (
+            Path(os.environ.get("XDG_CACHE_HOME") or (Path.home() / ".cache"))
+            / "pre-commit"
+        )
+    directory = precommitcachedirectory / "ktfmtprecommithook"
+    directory.mkdir(parents=True, exist_ok=True)
+    return directory
