e90e4a8cf6b815ffc39c6ae3eca2967b3d623ef9
David Hadley <davehadley@users.noreply.github.com>
2022-03-14 14:56:49 +0000

    Prepare 0.6.0. Update default ktfmt version to 0.34.

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,4 +2,4 @@ def test_version():
     import ktfmtprecommithook
 
     # Please remember to change the version in README.md when you change this.
-    assert ktfmtprecommithook.__version__ == "0.5.0"
+    assert ktfmtprecommithook.__version__ == "0.6.0"
