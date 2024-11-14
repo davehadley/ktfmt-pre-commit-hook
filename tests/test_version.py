7f6f8af5991334df12d10ff28fd40afc811cf578
David Hadley <davehadley@users.noreply.github.com>
2024-11-14 15:17:10 +0900

    Bump version number.

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,4 +2,4 @@ def test_version():
     import ktfmtprecommithook
 
     # Please remember to change the version in README.md when you change this.
-    assert ktfmtprecommithook.__version__ == "0.7.0"
+    assert ktfmtprecommithook.__version__ == "0.8.0"
